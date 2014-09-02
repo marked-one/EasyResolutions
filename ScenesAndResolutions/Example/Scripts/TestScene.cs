92b36e98e4af5c165c4cbe4da5548ee08979f020
Vladimir Klubkov <marked.one.more@gmail.com>
2014-09-02 17:57:30 +0400

    Link to github repo added to examples header

--- ScenesAndResolutions/Example/Scripts/TestScene.cs
+++ ScenesAndResolutions/Example/Scripts/TestScene.cs
@@ -1,5 +1,5 @@
 ﻿//-----------------------------------------------------------------------
-// <copyright file="TestSceneRotation.cs" company="">
+// <copyright file="TestSceneRotation.cs" company="https://github.com/marked-one/ScenesAndResolutionsPicker">
 //     Copyright © 2014 Vladimir Klubkov. All rights reserved.
 // </copyright>
 //-----------------------------------------------------------------------
